128c72764fb81bed1ddc1d2c2251b97251d52bf5
SandroSena <sandro340@hotmail.com>
2025-08-16 11:51:14 -0300

    Finish arrays-and-objects and buggatti on manipulating-objects

--- manipulating-objects/bugatti.js
+++ manipulating-objects/bugatti.js
@@ -1,27 +1,33 @@
 const myCar = {
-    make: 'Bugatti',
-    model: 'Bugatti La Voiture Noire',
-    year: 2019,
-    accidents: [
-        {
-            date: '3/15/2019',
-            damage_points: '5000',
-            atFaultForAccident: true
-        },
-        {
-            date: '7/4/2022',
-            damage_points: '2200',
-            atFaultForAccident: true
-        },
-        {
-            date: '6/22/2021',
-            damage_points: '7900',
-            atFaultForAccident: true
-        }
-    ]
-}
+  make: "Bugatti",
+  model: "Bugatti La Voiture Noire",
+  year: 2019,
+  accidents: [
+    {
+      date: "3/15/2019",
+      damage_points: "5000",
+      atFaultForAccident: true,
+    },
+    {
+      date: "7/4/2022",
+      damage_points: "2200",
+      atFaultForAccident: true,
+    },
+    {
+      date: "6/22/2021",
+      damage_points: "7900",
+      atFaultForAccident: true,
+    },
+  ],
+};
 
 // 1. Loop over the accidents array. Change atFaultForAccident from true to false.
+myCar.accidents.forEach((accident) => {
+  accident.atFaultForAccident = false;
+  console.log(accident);
+});
 
 // 2. Print the dated of my accidents
-
+myCar.accidents.forEach((accident) => {
+  console.log(accident.date);
+});
